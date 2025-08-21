e298cc262bebeb5e739e608fd2f7912bb5e3da2b
nabila tabaika <nabilatabaika070305@gmail.com>
2025-08-21 23:37:36 +0900

    commit pertama

---
+++ DART/project_nabila/bin/project_nabila.dart
@@ -0,0 +1,5 @@
+import 'package:project_nabila/project_nabila.dart' as project_nabila;
+
+void main(List<String> arguments) {
+  print('Hello world: ${project_nabila.calculate()}!');
+}
---
+++ DART/project_nabila/test/project_nabila_test.dart
@@ -0,0 +1,8 @@
+import 'package:project_nabila/project_nabila.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
