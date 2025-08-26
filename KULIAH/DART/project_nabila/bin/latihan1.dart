1d5e6054193b969a605f390cef8966b547d09149
nabila tabaika <nabilatabaika070305@gmail.com>
2025-08-26 11:44:30 +0900

    aplikasi biodata

---
+++ KULIAH/DART/project_nabila/bin/latihan1.dart
@@ -0,0 +1,13 @@
+void main () {
+    String name = "Nabila";
+    int age = 20;
+    double berat = 60;
+    num tinggi = 160;
+    dynamic prodi = "Teknik Informatika";
+
+    print ("name : $name");
+     print ("age : $age"); 
+      print ("berat : $berat");
+       print ("tinggi : $tinggi");
+        print ("prodi : $prodi");
+}
